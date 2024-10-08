ce4d99e56b130560881bb48161252e74eda99470
eduprog <eduprog@gmail.com>
2024-10-08 17:06:45 -0300

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OpenApi9Test.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[ApiExplorerSettings(GroupName = "controllers")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    [Route("/getbyidandname/{id}/{name}")]
+    public string GetByIdAndName(RouteParamsContainer paramsContainer)
+    {
+        return paramsContainer.Id + "_" + paramsContainer.Name;
+    }
+
+    [HttpPost]
+    [Route("/forms")]
+    public IActionResult PostForm([FromForm] MvcTodo todo)
+    {
+        return Ok(todo);
+    }
+
+    public class RouteParamsContainer
+    {
+        [FromRoute]
+        public int Id { get; set; }
+
+        [FromRoute]
+        [MinLength(5)]
+        [UnconditionalSuppressMessage("Trimming", "IL2026:RequiresUnreferencedCode", Justification = "MinLengthAttribute works without reflection on string properties.")]
+        public string? Name { get; set; }
+    }
+
+    public record MvcTodo(string Title, string Description, bool IsCompleted);
+}
