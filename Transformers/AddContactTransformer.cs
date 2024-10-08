abed6eca3d75671b20295926b9dcf2a55f0d358b
eduprog <eduprog@gmail.com>
2024-10-08 17:06:45 -0300

    Add project files.

---
+++ Transformers/AddContactTransformer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace Sample.Transformers;
+
+public sealed class AddContactTransformer : IOpenApiDocumentTransformer
+{
+    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        document.Info.Contact = new OpenApiContact
+        {
+            Name = "OpenAPI Enthusiast",
+            Email = "iloveopenapi@example.com"
+        };
+        return Task.CompletedTask;
+    }
+}
